0eab16e0952bb852bb87ceef5a4a49808bb45589
Masencod <sajjad.ghorashy@gmail.com>
2023-07-19 12:15:33 +0330

    is working need to handle wins and add animations

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,134 +1,61 @@
+import { useEffect, useState } from "react";
 import Players from "./Players";
+import clsx from "clsx";
 
 export default function Grid() {
+  const [players, setPlayers] = useState([]);
 
+  const handleGetPlayers = (prop) => {
+    setPlayers(prop);
+  };
+  useEffect(() => {
+    console.log(players);
+  }, [players]);
 
+  const row1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const row2 = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11];
+  const row3 = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  const row4 = [40, 39, 38, 37, 36, 35, 34, 33, 32, 31];
+  const row5 = [41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+  const row6 = [60, 59, 58, 57, 56, 55, 54, 53, 52, 51];
+  const row7 = [61, 62, 63, 64, 65, 66, 67, 68, 69, 70];
+  const row8 = [80, 79, 78, 77, 76, 75, 74, 73, 72, 71];
+  const row9 = [81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
+  const row10 = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91];
+
+  // Function to check if a player is present in a cell
+  const isPlayerInCell = (cellId) => {
+    return players.some((player) => player.position === cellId);
+  };
+
+  // Function to get the players' images in a cell
+  const getPlayerImagesInCell = (cellId) => {
+    return players
+      .filter((player) => player.position === cellId)
+      .map((player) => player.img);
+  };
 
   return (
-    <div className="flex flex-col  justify-center md:items-center md:mt-10 ">
+    <div className="flex flex-col justify-center md:items-center md:mt-10 ">
       <div className="grid board w-[100%] md:max-w-[700px] md:w-[70%] aspect-square bg-[url('./bg.jpg')] bg-contain">
-        <div className=" grid rows gr" id="row1">
-          <div id="100"></div>
-          <div id="99"></div>
-          <div id="98"></div>
-          <div id="97"></div>
-          <div id="96"></div>
-          <div id="95"></div>
-          <div id="94"></div>
-          <div id="93"></div>
-          <div id="92"></div>
-          <div id="91"></div>
-        </div>
-        <div className=" grid rows" id="row2">
-          <div id="81"></div>
-          <div id="82"></div>
-          <div id="83"></div>
-          <div id="84"></div>
-          <div id="85"></div>
-          <div id="86"></div>
-          <div id="87"></div>
-          <div id="88"></div>
-          <div id="89"></div>
-          <div id="90"></div>
-        </div>
-        <div className=" grid rows" id="row3">
-          <div id="80"></div>
-          <div id="79"></div>
-          <div id="78"></div>
-          <div id="77"></div>
-          <div id="76"></div>
-          <div id="75"></div>
-          <div id="74"></div>
-          <div id="73"></div>
-          <div id="72"></div>
-          <div id="71"></div>
-        </div>
-        <div className=" grid rows" id="row4">
-          <div id="61"></div>
-          <div id="62"></div>
-          <div id="63"></div>
-          <div id="64"></div>
-          <div id="65"></div>
-          <div id="66"></div>
-          <div id="67"></div>
-          <div id="68"></div>
-          <div id="69"></div>
-          <div id="70"></div>
-        </div>
-        <div className=" grid rows" id="row5">
-          <div id="60"></div>
-          <div id="59"></div>
-          <div id="58"></div>
-          <div id="57"></div>
-          <div id="56"></div>
-          <div id="55"></div>
-          <div id="54"></div>
-          <div id="53"></div>
-          <div id="52"></div>
-          <div id="51"></div>
-        </div>
-        <div className=" grid rows" id="row6">
-          <div id="41"></div>
-          <div id="42"></div>
-          <div id="43"></div>
-          <div id="44"></div>
-          <div id="45"></div>
-          <div id="46"></div>
-          <div id="47"></div>
-          <div id="48"></div>
-          <div id="49"></div>
-          <div id="50"></div>
-        </div>
-        <div className=" grid rows" id="row7">
-          <div id="40"></div>
-          <div id="39"></div>
-          <div id="38"></div>
-          <div id="37"></div>
-          <div id="36"></div>
-          <div id="35"></div>
-          <div id="34"></div>
-          <div id="33"></div>
-          <div id="32"></div>
-          <div id="31"></div>
-        </div>
-        <div className=" grid rows" id="row8">
-          <div id="21"></div>
-          <div id="22"></div>
-          <div id="23"></div>
-          <div id="24"></div>
-          <div id="25"></div>
-          <div id="26"></div>
-          <div id="27"></div>
-          <div id="28"></div>
-          <div id="29"></div>
-          <div id="30"></div>
-        </div>
-        <div className=" grid rows" id="row9">
-          <div id="20"></div>
-          <div id="19"></div>
-          <div id="18"></div>
-          <div id="17"></div>
-          <div id="16"></div>
-          <div id="15"></div>
-          <div id="14"></div>
-          <div id="13"></div>
-          <div id="12"></div>
-          <div id="11"></div>
-        </div>
-        <div className=" grid rows" id="row10">
-          <div id="1"></div>
-          <div id="2"></div>
-          <div id="3"></div>
-          <div id="4"></div>
-          <div id="5"></div>
-          <div id="6"></div>
-          <div id="7"></div>
-          <div id="8"></div>
-          <div id="9"></div>
-          <div id="10"></div>
-        </div>
+        {[row10, row9, row8, row7, row6, row5, row4, row3, row2, row1].map(
+          (row, rowIndex) => (
+            <div key={rowIndex} className="grid rows">
+              {row.map((cellId) => (
+                <div key={cellId} id={cellId} className="cell flex justify-center items-center">
+                  {isPlayerInCell(cellId) &&
+                    getPlayerImagesInCell(cellId).map((imgSrc, index) => (
+                      <img
+                      className={clsx("max-w-[30px]")}
+                      key={index} src={imgSrc} alt="player" />
+                    ))}
+                </div>
+              ))}
+            </div>
+          )
+        )}
       </div>
-      <Players />
+      <Players getPlayers={handleGetPlayers} />
     </div>
   );
 }
--- src/Components/Players.jsx
+++ src/Components/Players.jsx
@@ -1,41 +1,9 @@
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 import ReactModal from "react-modal";
 
-export default function Players() {
+export default function Players({ getPlayers }) {
   // variables
-  let defaultPlayers = [
-    {
-      id: "1",
-      name: "player 1",
-      color: "red",
-      img: "./red.png",
-    },
-    {
-      id: "2",
-      name: "player 2",
-      color: "blue",
-      img: "./blue.png",
-    },
-    {
-      id: "3",
-      name: "player 3",
-      color: "purple",
-      img: "./purple.png",
-    },
-    {
-      id: "4",
-      name: "player 4",
-      color: "green",
-      img: "./green.png",
-    },
-  ];
-  const [positions, setPositions] = useState({});
-  const [players, setPlayers] = useState(defaultPlayers);
-  const [activePlayer, setActivePlayer] = useState(players[0])
-  const [isOpenModal, setIsOpenModal] = useState(true);
-  const [playerAmount, setPlayerAmount] = useState(2);
-
-  //custom style
   const customStyles = {
     content: {
       top: "50%",
@@ -47,18 +15,152 @@ export default function Players() {
     },
   };
 
+  let defaultPlayers = [
+    {
+      id: "1",
+      name: "player 1",
+      color: "cyan",
+      img: "./cyan.png",
+      position: 0,
+    },
+    {
+      id: "2",
+      name: "player 2",
+      color: "blue",
+      img: "./blue.png",
+      position: 0,
+    },
+    {
+      id: "3",
+      name: "player 3",
+      color: "purple",
+      img: "./purple.png",
+      position: 0,
+    },
+    {
+      id: "4",
+      name: "player 4",
+      color: "green",
+      img: "./green.png",
+      position: 0,
+    },
+  ];
+
+  const [diceAmount, setDiceAmount] = useState(1);
+  const [players, setPlayers] = useState(defaultPlayers);
+  const [activePlayer, setActivePlayer] = useState(players[0]);
+  const [isOpenModal, setIsOpenModal] = useState(true);
+  const [playerAmount, setPlayerAmount] = useState(2);
+  const [winner, setWinner] = useState({})
+
+  //handlers
   const handlePlayers = () => {
-    if (playerAmount !== 4) {
+    if (playerAmount != 4) {
       setPlayers(players.slice(0, -(4 - playerAmount)));
     }
   };
 
-  useEffect(() => {
-    console.log(activePlayer);
-  }, [players]);
+  const handleDice = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    setDiceAmount(randomNumber);
+    let newPosition = posRules(activePlayer.position + randomNumber);
 
+    const updatedPlayers = players.map((player) => {
+      if (player.id === activePlayer.id) {
+        return { ...player, position: newPosition };
+      }
+      return player;
+    });
+
+    setPlayers(updatedPlayers);
+    const handleWin = (arr) => {
+      const atEnd = arr.filter(e => e.position == 100)
+      if (atEnd.length > 0) {
+        setWinner(atEnd);
+      }
+    }
+    handleWin(updatedPlayers);
+
+    if (activePlayer.id === playerAmount) {
+      setActivePlayer(players[0]);
+      getPlayers(updatedPlayers);
+    } else {
+      setActivePlayer(players[activePlayer.id]);
+      getPlayers(updatedPlayers);
+    }
+  };
+
+  //rules
+  const posRules = (pos) => {
+    switch (pos) {
+      case 3:
+        return 20;
+      case 6:
+        return 14;
+      case 8:
+        return 4;
+      case 11:
+        return 28;
+      case 15:
+        return 34;
+      case 17:
+        return 74;
+      case 18:
+        return 1;
+      case 22:
+        return 37;
+      case 26:
+        return 10;
+      case 38:
+        return 59;
+      case 39:
+        return 5;
+      case 49:
+        return 67;
+      case 51:
+        return 7;
+      case 54:
+        return 36;
+      case 56:
+        return 1;
+      case 57:
+        return 76;
+      case 60:
+        return 23;
+      case 61:
+        return 78;
+      case 73:
+        return 86;
+      case 75:
+        return 28;
+      case 81:
+        return 98;
+      case 83:
+        return 45;
+      case 85:
+        return 59;
+      case 88:
+        return 91;
+      case 92:
+        return 25;
+      case 97:
+        return 87;
+      case 99:
+        return 63;
+    }
+    return pos;
+  };
   return (
-    <div className="w-full md:max-w-[700px] md:w-[70%]">
+    <div className="w-full mt-10 md:max-w-[700px] md:w-[70%]">
+      {winner[name] &&       <ReactModal
+        isOpen={isOpenModal}
+        style={customStyles}
+        ariaHideApp={false}
+      >
+        <div>
+          <p><span className='text-orange-500 text-xl'>{winner.name}</span> IS THE WINNER</p>
+        </div>
+      </ReactModal>}
       <ReactModal
         isOpen={isOpenModal}
         onAfterClose={() => handlePlayers()}
@@ -91,19 +193,59 @@ export default function Players() {
         </div>
       </ReactModal>
       <div className="flex justify-between mt-2 w-full">
-        <div className="flex flex-wrap flex-col flex-1 w-[100px] w-full gap-y-2">
+        <div className="grid grid-flow-col flex-1 w-[100px] ml-2 gap-y-2">
           {players.map((e) => {
+            if (e.position != 0) {
+              return null;
+            }
             return (
-              <img className="w-[10%]" key={e.id} src={e.img} alt="tokens" />
-            )
+              <img
+                className={clsx("max-w-[30px]", {
+                  "w-[50%]": playerAmount == 4 && !isOpenModal,
+                  "w-[40%]": playerAmount == 3 && !isOpenModal,
+                  "w-[30%]": playerAmount == 2 && !isOpenModal,
+                  "w-[49%]": isOpenModal,
+                })}
+                key={e.id}
+                src={e.img}
+                alt="tokens"
+              />
+            );
           })}
         </div>
-        <div className={`flex-1 text-${activePlayer.color}-500`}>
-          <p>{activePlayer.name}</p>
+        <div className={`flex-1 flex items-center justify-center`}>
+          <p className="flex gap-x-2 text-xl font-semibold">
+            Turn:
+            <span
+              className={clsx("", {
+                "text-cyan-500": activePlayer.color === "cyan",
+                "text-green-500": activePlayer.color === "green",
+                "text-purple-500": activePlayer.color === "purple",
+                "text-blue-500": activePlayer.color === "blue",
+              })}
+            >
+              {activePlayer.name}
+            </span>
+          </p>
         </div>
-        <div className="flex-1">
-          <div>awdawdadw</div>
-          <button>tasss</button>
+        <div className="flex-1 flex flex-col items-end justify-center mr-4">
+          <div className="flex flex-col items-center">
+            <div className="p-2">{diceAmount}</div>
+            <button
+              className={clsx(
+                "py-2 px-5 rounded-lg shadow-md hover:shadow-xl transition-all duration-200 ease-in-out",
+                {
+                  "bg-red-300": activePlayer.color === "red",
+                  "bg-green-300": activePlayer.color === "green",
+                  "bg-purple-300": activePlayer.color === "purple",
+                  "bg-blue-300": activePlayer.color === "blue",
+                }
+              )}
+              onClick={handleDice}
+            >
+              Roll
+            </button>
+          </div>
         </div>
       </div>
     </div>
